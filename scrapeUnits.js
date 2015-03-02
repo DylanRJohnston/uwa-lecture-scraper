71390f5534703975216bba40df6ec4629218357f
Dylan R. Johnston <Dylan.R.Johnston@gmail.com>
2015-03-02 14:06:19 +0800

    Added MIT license and error handling on the first fetch.

--- scrapeUnits.js
+++ scrapeUnits.js
@@ -1,3 +1,11 @@
+/*
+ * Scrapes UWA lecture capture system for Unit information
+ * Retrieves unit codes, names, and unit hash id's.
+ *
+ * Distributed under the MIT License.
+ * See http://opensource.org/licenses/MIT
+ */
+
 var async       = require('async');
 var fs          = require('fs');
 var request     = require('request');
@@ -10,13 +18,18 @@ var url = 'http://echo360-test.its.uwa.edu.au/echocontent/sections/';
 request(
     url,
     function(error, response, body) {
+        if (error) {
+            console.log("Error fetching " + url + "\nAborting.");
+            return;
+        }
+
         var $ = cheerio.load(body);
         var units = [];
         var bar = new ProgressBar('Pulling [:bar] :current/:total :percent :eta', {total: $('td > a').length});
 
         async.eachLimit(
             $('td > a').toArray(),
-            1,
+            10,
             function(element, callback) {
                 request(
                     url + element.attribs.href + 'section.xml',
@@ -37,7 +50,7 @@ request(
                         });
 
                         bar.tick(1);
-                        callback(null);
+                        callack(null);
                     }
                 );
             },
